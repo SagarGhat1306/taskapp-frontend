d86ce35276a72437de35ac3d59646fa64463adbe
Sagar Ghat <sagarghat2004@gmail.com>
2025-10-30 12:21:40 +0530

    Updated the login  file api link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ export default function Login({ onLogin }) {
 
 const onSubmit = async (data) => {
   try {
-    const res = await fetch('https://taskapp-backend.onrender.com/api/auth/login', {
+    const res = await fetch(' https://taskapp-backend-wo5y.onrender.com/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register({ onRegister }) {
 
   const onSubmit = async (data) => {
   try {
-    const res = await fetch('https://taskapp-backend.onrender.com/api/auth/register', {
+    const res = await fetch(' https://taskapp-backend-wo5y.onrender.com/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
