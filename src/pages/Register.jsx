8291741bebf462d97d9ee58af4975f6d59a5c859
Sagar Ghat <sagarghat2004@gmail.com>
2025-10-30 11:36:00 +0530

    Updated the register file api link for render backend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register({ onRegister }) {
 
   const onSubmit = async (data) => {
   try {
-    const res = await fetch('http://localhost:5000/api/auth/register', {
+    const res = await fetch('https://taskapp-backend.onrender.com/api/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
